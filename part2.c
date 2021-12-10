7e8286799e841d00cf519ce9e372d456636b299a
AnaEstrada1996 <ana_estrada1@yahoo.com>
2021-12-10 04:30:28 +0000

    abAppend

--- part2.c
+++ part2.c
@@ -1,5 +1,5 @@
 /*** includes **/
-
+#include <string.h>
 #include <errno.h>
 #include <ctype.h>
 #include <stdio.h>
@@ -95,6 +95,21 @@ struct abuf{
 	char *b;
 	int len;
 };
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len){
+	char *new = realloc(ab->b, ab->len + len);
+	
+	if (new == NULL) return;
+	memcpy(&new[ab->len], s, len);
+	ab->b = new;
+	ab->len += len;
+}
+
+void abFree(struct abuf *ab){
+	free(ab->b);
+}
 
 /*** output ***/
 void editorDrawRows(){
