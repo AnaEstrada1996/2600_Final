58858679f855e33a11ebd63f886ddfcf4ef78a27
AnaEstrada1996 <ana_estrada1@yahoo.com>
2021-12-10 23:33:14 +0000

    prevent inserting special characters

--- part2.c
+++ part2.c
@@ -24,6 +24,7 @@
 #define KILO_TAB_STOP 8
 
 enum editorKey{
+	BACKSPACE = 127;
 	ARROW_LEFT = 1000,
 	ARROW_RIGHT,
 	ARROW_UP,
@@ -450,6 +451,9 @@ void editorProcessKeypress(){
 	int c = editorReadKey();
 
 	switch(c){
+		case '\r':
+			/*TODO*/
+			break;
 		case CTRL_KEY('q'):
 			write(STDOUT_FILENO, "\x1b[2J", 4);
 			write(STDOUT_FILENO, "\x1b[H", 3);
@@ -462,7 +466,11 @@ void editorProcessKeypress(){
 			if (E.cy < E.numrows)
 				E.cx = E.row[E.cy].size;
 			break;
-
+		case BACKSPACE:
+		case CTRL_KEY('h'):
+		case DEL_KEY:
+			/* TODO */ 
+			break;
 		case PAGE_UP:
 		case PAGE_DOWN:
 			{
@@ -484,6 +492,9 @@ void editorProcessKeypress(){
 		case ARROW_LEFT:
 		case ARROW_RIGHT:
 			editorMoveCursor(c);
+			break;
+		case CTRL_KEY('l'):
+		case '\x1b':
 			break;
 
 		default:
