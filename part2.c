9322968f053153c11a8122b46e5a96bf1daa75bf
AnaEstrada1996 <ana_estrada1@yahoo.com>
2021-12-11 03:52:10 +0000

    add an array to the erow struct named hl, which stands for highlight

--- part2.c
+++ part2.c
@@ -45,6 +45,7 @@ typedef struct erow{
 	int rsize;
 	char *chars;
 	char *render;
+	unsigned char *hl;
 }erow;
 
 struct editorConfig{
@@ -251,7 +252,8 @@ void editorInsertRow(int at, char *s, size_t len){
 	E.row[at].chars[len] = '\0';
 
 	E.row[at].rsize = 0;
-	E.row[at].render = NULL;	
+	E.row[at].render = NULL;
+	E.row[at].hl = NULL;	
 	editorUpdateRow(&E.row[at]);
 
 	E.numrows++;
@@ -261,6 +263,7 @@ void editorInsertRow(int at, char *s, size_t len){
 void editorFreeRow(erow *row){
 	free(row->render);
 	free(row->chars);
+	free(row->hl);
 }
 
 void editorDelRow(int at){
