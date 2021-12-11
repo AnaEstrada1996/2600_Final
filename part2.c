08e0b91d8cc9a57b803b76c374bb64c589d18ee3
AnaEstrada1996 <ana_estrada1@yahoo.com>
2021-12-11 03:23:25 +0000

    pass null to editorprompt

--- part2.c
+++ part2.c
@@ -392,7 +392,7 @@ void editorOpen(char *filename){
 
 void editorSave(){
 	if(E.filename == NULL) {
-		E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+		E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
 		if(E.filename == NULL){
 			editorSetStatusMessage("Save aborted");
 			return;
@@ -422,7 +422,7 @@ void editorSave(){
 
 /*** find ***/
 void editorFind(){
-	char *query = editorPrompt("SearchL %s (ESC to cancel)");
+	char *query = editorPrompt("SearchL %s (ESC to cancel)", NULL);
 	if(query == NULL) return;
 
 	int i;
