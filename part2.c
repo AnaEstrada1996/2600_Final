d60052390404c74c8bb0ca952324a185252f9557
AnaEstrada1996 <ana_estrada1@yahoo.com>
2021-12-11 05:18:46 +0000

    highlight code

--- part2.c
+++ part2.c
@@ -228,12 +228,24 @@ void editorUpdateSyntax(erow *row){
 	row->hl = realloc(row->hl, row->rsize);
 	memset(row->hl, HL_NORMAL, row->rsize);
 	if(E.syntax == NULL) return;
+
+	char *scs = E.syntax->singleline_comment_start;
+	int scs_len = scs ? strlen(scs) : 0;
+
+
 	int prev_sep = 1;
 
 	int i = 0; 
 	while(i < row->rsize){	
 		char c = row->render[i];
 		unsigned char prev_hl = (i > 0 ) ? row->hl[i-1] : HL_NORMAL;
+
+		if(scs_len && !in_string){
+			if(!strncmp(&row->render[i], scs, scs_len)){
+				memset(&row->hl[i], HL_COMMENT, row->rsize - i);
+				break;
+			}
+		}
 
 		if(E.system->flags & HL_HIGHLIGHT_NUMBERS || HL_HIGHLIGHT_STRINGS){
 			if(in string){
