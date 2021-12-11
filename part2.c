4a0963669471458dff40eea9b7f94b26699d6b58
AnaEstrada1996 <ana_estrada1@yahoo.com>
2021-12-11 04:38:15 +0000

    support highlighting nums that contain decimal points

--- part2.c
+++ part2.c
@@ -207,7 +207,7 @@ void editorUpdateSyntax(erow *row){
 		char c = row->render[i];
 		unsigned char prev_hl = (i > 0 ) ? row->hl[i-1] : HL_NORMAL;
 
-		if(isdigit(c) && (prev_sep || prev_hl == HL_NUMBNER)){
+		if(isdigit(c) && (prev_sep || prev_hl == HL_NUMBNER)) || (c == '.' && prev_hl == HL_NUMBER)){
 			row->hl[i] = HL_NUMBER;
 			i++;
 			prev_sep = 0;
