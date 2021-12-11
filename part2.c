a2d984a58f680199f37d5942146964b65b2ef4d7
AnaEstrada1996 <ana_estrada1@yahoo.com>
2021-12-11 00:28:03 +0000

    quit confirmation

--- part2.c
+++ part2.c
@@ -22,6 +22,7 @@
 #define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+#define KILO_QUIT_TIMES 3
 #define KILO_TAB_STOP 8
 
 enum editorKey{
@@ -504,6 +505,7 @@ void editorMoveCursor(int key){
 }
 
 void editorProcessKeypress(){
+	static int quit_times = KILO_QUIT_TIMES;
 	int c = editorReadKey();
 
 	switch(c){
@@ -511,6 +513,11 @@ void editorProcessKeypress(){
 			/*TODO*/
 			break;
 		case CTRL_KEY('q'):
+			if(E.dirty && quit_times > 0 ){
+				editorSetStatusMessage("WARNING!!! File has unsaved changes. " "Press Ctrl-Q %d more times to quit.", quit_times);
+				quit_times--;
+				return;
+			}
 			write(STDOUT_FILENO, "\x1b[2J", 4);
 			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
@@ -560,6 +567,7 @@ void editorProcessKeypress(){
 			editorInsertChar(c);
 			break;
 	}
+	quit_times = KILO_QUIT_TIMES;
 }
 
 /***init ***/
